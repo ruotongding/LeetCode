236fc63d7c8b5cf9d179806ab25fe36ac3a95c77
Ruotong Ding <ruotong.ding@student.manchester.ac.uk>
2022-01-07 02:22:36 +0000

    合并区间

---
+++ 合并区间.py
@@ -0,0 +1,37 @@
+# 以数组 intervals 表示若干个区间的集合，其中单个区间为 intervals[i] = [starti, endi] 。请你合并所有重叠的区间，并返回一个不重叠的区间数组，该数组需恰好覆盖输入中的所有区间。
+#
+# 示例 1：
+#
+# 输入：intervals = [[1,3],[2,6],[8,10],[15,18]]
+# 输出：[[1,6],[8,10],[15,18]]
+# 解释：区间 [1,3] 和 [2,6] 重叠, 将它们合并为 [1,6].
+# 示例 2：
+#
+# 输入：intervals = [[1,4],[4,5]]
+# 输出：[[1,5]]
+# 解释：区间 [1,4] 和 [4,5] 可被视为重叠区间。
+#
+# 来源：力扣（LeetCode）
+# 链接：https://leetcode-cn.com/problems/merge-intervals
+
+
+class Solution(object):
+    def merge(self, intervals):
+        """
+        :type intervals: List[List[int]]
+        :rtype: List[List[int]]
+        """
+        intervals.sort()
+        no_overla = [intervals[0]]
+        L, R = 1, 0
+        while L<len(intervals):
+            # no overlap
+            if no_overla[R][1]<intervals[L][0]:
+                no_overla.append(intervals[L])
+                L+=1
+                R+=1
+            else:
+                no_overla[R]=[no_overla[R][0],max(no_overla[R][1], intervals[L][1])]
+                L+=1
+
+        return no_overla
